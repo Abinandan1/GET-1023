e1f6a4fd26135092ac62226e02e9b8220f10004b
Abinandan A T <79516434+Abinandan1@users.noreply.github.com>
2022-07-14 17:21:45 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -2,5 +2,6 @@ class Sample{
   public static void main(String args[]){
     System.out.println("Hello World");
     System.out.println("New Hello World");
+    System.out.println("New Poll SCM Hello World");
   } 
 }
